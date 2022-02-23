8150bfed3ae409f474adbcabdbd5b6cffdaf6f2c
sieukim <s2lver@naver.com>
2022-02-24 02:34:51 +0900

    폰트 변경하기

--- chapter04/image_font_add_app/lib/imageWidget.dart
+++ chapter04/image_font_add_app/lib/imageWidget.dart
@@ -19,7 +19,12 @@ class _ImageWidgetAppState extends State<ImageWidgetApp> {
               child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Image.asset('image/flutter.png', width: 200, height: 100)
+              Image.asset('image/flutter.png', width: 200, height: 100),
+              const Text(
+                'Hello Flutter',
+                style: TextStyle(
+                    fontFamily: 'Pacifico', fontSize: 30, color: Colors.blue),
+              )
             ],
           )),
         ));
