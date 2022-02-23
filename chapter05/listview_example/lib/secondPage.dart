9477a62ecd698c04a320f8aa6faed366091218db
sieukim <s2lver@naver.com>
2022-02-24 05:36:55 +0900

    동물 추가 UI 만들기

--- chapter05/listview_example/lib/secondPage.dart
+++ chapter05/listview_example/lib/secondPage.dart
@@ -1,18 +1,115 @@
 import 'package:flutter/material.dart';
+
 import './animalItem.dart';
 
-class SecondPage extends StatelessWidget {
-  // animalItem.dart 내 animalList의 데이터를 받아올건데
-  // SecondPage 객체 생성시에도 해당 목록이 있어야 하므로 final로 선언한다.
+class SecondPage extends StatefulWidget {
+  // animalList
   final List<Animal>? animalList;
+
   // animalList를 생성자에 추가
   const SecondPage({Key? key, this.animalList}) : super(key: key);
 
   @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  // Textfield Controller
+  final nameController = TextEditingController();
+
+  // Radio value
+  int? _radioValue = 0;
+
+  bool? flyExist = false;
+  String? _imagePath;
+
+  // Radio onChanged 핸들러
+  _radioChange(int? value) {
+    setState(() {
+      _radioValue = value;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('두 번째 페이지'),
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                controller: nameController,
+                keyboardType: TextInputType.text,
+                maxLines: 1,
+              ),
+              Row(
+                children: <Widget>[
+                  Radio(
+                      value: 0,
+                      groupValue: _radioValue,
+                      onChanged: _radioChange),
+                  const Text('양서류'),
+                  Radio(
+                      value: 1,
+                      groupValue: _radioValue,
+                      onChanged: _radioChange),
+                  const Text('파충류'),
+                  Radio(
+                      value: 2,
+                      groupValue: _radioValue,
+                      onChanged: _radioChange),
+                  const Text('포유류'),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Row(
+                children: <Widget>[
+                  const Text('날 수 있나요?'),
+                  Checkbox(
+                    value: flyExist,
+                    onChanged: (bool? check) {
+                      setState(() {
+                        flyExist = check;
+                      });
+                    },
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              Row(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Image.asset('images/bee.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'images/bee.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('images/cat.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'images/cat.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('images/cow.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'images/cow.png';
+                    },
+                  ),
+                  GestureDetector(
+                    child: Image.asset('images/dog.png', width: 80),
+                    onTap: () {
+                      _imagePath = 'images/dog.png';
+                    },
+                  ),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text('동물 추가하기'))
+            ],
+          ),
+        ),
       ),
     );
   }
